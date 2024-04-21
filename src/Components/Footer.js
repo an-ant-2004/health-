4ba312219b41d4709e8591d9188ed6afe1ec665f
an-ant-2004 <83494101+an-ant-2004@users.noreply.github.com>
2024-04-21 21:09:31 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "../Styles/Footer.css";
+import SubscribeNewsletter from "./SubscribeNewsletter";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-section">
+      <div className="footer-container">
+        <div className="ft-info">
+          <div className="ft-info-p1">
+            <p className="ft-title">
+              Health <span className="ft-sign">+</span>
+            </p>
+            <p className="ft-description">
+              Talk to online doctors and get medical advice, online
+              prescriptions, refills and medical notes within minutes. On-demand
+              healthcare services at your fingertips.
+            </p>
+          </div>
+
+          <SubscribeNewsletter />
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Services</p>
+          <ul className="ft-list-items">
+            <li>
+              <a href="#services">Emergency Care</a>
+            </li>
+            <li>
+              <a href="#services">Heart Disease</a>
+            </li>
+            <li>
+              <a href="#services">Dental Care</a>
+            </li>
+            <li>
+              <a href="#services">Prescription</a>
+            </li>
+            <li>
+              <a href="#services">Insights for doctors</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Legal</p>
+          <ul className="ft-list-items">
+            <li>
+              <Link to={"/legal"}>General Info</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Terms of Services</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Consultations</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>How it Works</Link>
+            </li>
+          </ul>
+        </div>
+
+        
+      </div>
+
+      
+            
+    </div>
+  );
+}
+
+export default Footer;
